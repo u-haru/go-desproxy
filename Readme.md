9534874e5f5fe3bf6145191f0c38ed9945cf609e
u-haru <40634644+u-haru@users.noreply.github.com>
2020-11-27 18:05:37 +0900

    First commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# go-desproxy
+
+プロキシ環境下でCONNECTメゾットを用いてTCPリダイレクトを行うサンプル
+
+
+
+Usage:
+  -p int
+        local port (default 8080)
+  -r string
+        remote host:port
+  -u string
+        username:password
+  -x string
+        Proxy:port (default "10.1.16.8:8080")
---
+++ src/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+var (
+	localport          = 8080
+	proxyHost          = ""
+	remoteHost         = ""
+	proxyAuthorization = ""
+)
+
+func HandleRequest(clientConn net.Conn) {
+	if proxyConn, err := net.Dial("tcp", proxyHost); err != nil {
+		log.Fatal(err)
+	} else {
+		url, err := url.Parse("http://" + remoteHost)
+		if err != nil {
+			log.Fatal(err)
+		}
+		addr, err := net.ResolveIPAddr("ip4", url.Hostname())
+		if err != nil {
+			log.Fatal(err)
+		}
+		req, err := http.NewRequest("CONNECT", "https://"+remoteHost, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.Host = fmt.Sprintf("%s", addr.String()+":"+url.Port())
+		req.Header.Set("Proxy-Authorization", proxyAuthorization)
+		req.Write(proxyConn)
+		loop := true
+		br := bufio.NewReader(proxyConn)
+		for loop {
+			b, err := br.ReadByte()
+			if b == '\n' {
+				loop = false
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		go func() {
+			io.Copy(clientConn, proxyConn)
+			proxyConn.Close()
+		}()
+		go func() {
+			io.Copy(proxyConn, clientConn)
+			clientConn.Close()
+		}()
+	}
+}
+
+func main() {
+	_proxyUser := flag.String("u", "", "username:password")
+	_localport := flag.Int("p", 8080, "local port")
+	_remoteHost := flag.String("r", "", "remote host:port")
+	_proxyHost := flag.String("x", "10.1.16.8:8080", "Proxy:port")
+	flag.Parse()
+	localport = *_localport
+	remoteHost = *_remoteHost
+	proxyHost = *_proxyHost
+
+	proxyAuthorization = "Basic " + base64.StdEncoding.EncodeToString([]byte(*_proxyUser))
+	proxyUrlString := fmt.Sprintf("http://%s@%s", strings.Replace(url.QueryEscape(*_proxyUser), "%3A", ":", 1), proxyHost)
+	proxyUrl, err := url.Parse(proxyUrlString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.DefaultTransport = &http.Transport{Proxy: http.ProxyURL(proxyUrl)}
+	listener, err := net.Listen("tcp", "localhost:"+"443")
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer listener.Close()
+	fmt.Println("Listening on localhost:")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		go HandleRequest(conn)
+	}
+}
